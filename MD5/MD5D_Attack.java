7812d610315eebd3c6c6cc4ad9c2e169a5d01f8b
Ahmed <ahmn21@hotmail.com>
2021-04-07 03:33:00 +0500

    Completed

--- MD5/MD5D_Attack.java
+++ MD5/MD5D_Attack.java
@@ -21,29 +21,82 @@ public class MD5D_Attack {
         return hash;
     }
 
-    public static void bruteForce(String hash) {
+    public static String next(String s) {
+        int length = s.length();
+        char c = s.charAt(length - 1);
 
+        if (c == 'z')
+            return length > 1 ? next(s.substring(0, length - 1)) + 'a' : "aa";
+
+        return s.substring(0, length - 1) + ++c;
+    }
+
+    public static String stringInit(int length, String str) {
+        for(int i=0; i<length; i++)
+        {
+            str += "a";
+        }
+        return str;
+    }
+
+    public static void bruteForce(String pass) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        boolean found;
+        int length;
+        String guessString = "";
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter length of password: ");
+            length = sc.nextInt();
+            if (length < 1) {
+                System.out.println("Length cannot be less than 1");
+            } else {
+                guessString = stringInit(length, "");
+                break;
+            }
+        }
+        sc.close();
+
+        while (true) {
+            String Match = MD5(guessString);
+            System.out.println(guessString);
+            if (Match.equals(pass)) {
+                System.out.println("Password found: \n" + guessString);
+                found = true;
+                break;
+            }
+            guessString = next(guessString);
+        }
+        if (!found) {
+            System.out.println("Password not Found");
+            return;
+        }
     }
 
     public static void main(String[] args) throws UnsupportedEncodingException, NoSuchAlgorithmException {
         // Main functionality of the code
-        System.out.print("Enter the hash to find:");
+        System.out.print("Enter the hash to find: ");
         Scanner S = new Scanner(System.in);
         String pass = S.nextLine();
         try {
             String path = "";
-            String choice;
+            int choice;
             while (true) {
-                System.out.print("\n1. Dictionary attack\n2. Common passwords attack\n3. Brute force attack\nInput: ");
-                choice = S.nextLine();
-                if (choice == "1") {
+                System.out.print("\n1. Dictionary attack\n2. Common passwords attack\n3. Brute force attack\n0. EXIT\nInput: ");
+                choice = S.nextInt();
+                if (choice == 1) {
                     path += "dictionary.txt";
                     break;
-                } else if (choice == "2") {
+                } else if (choice == 2) {
                     path += "commonpasswords.txt";
                     break;
-                } else if (choice == "3") {
+                } else if (choice == 3) {
                     bruteForce(pass);
+                    S.close();
+                    return;
+                }
+                else if(choice == 0)
+                {
                     S.close();
                     return;
                 } else {
